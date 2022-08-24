6a448287ea1750d14514d39ec10bf0ae16fb8c58
Fernando Mendioroz <fernando.mendioroz@gmail.com>
2022-08-23 23:54:25 -0300

    Temporary remmoval of almost all files

--- examples/gmlc-diameter-service/src/main/java/org/mobicents/servers/diameter/gmlc/queue/IGmlcSubscriber.java
+++
@@ -1,8 +0,0 @@
-package org.mobicents.servers.diameter.gmlc.queue;
-
-import org.mobicents.servers.diameter.gmlc.queue.data.LocationInformation;
-
-public interface IGmlcSubscriber {
-
-    void notificationReceived(LocationInformation locationInformation);
-}
